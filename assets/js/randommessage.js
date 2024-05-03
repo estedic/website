ae1af7bbbab260508d2d83d73004d60fb2f3f87d
Bram Bregman <bregman@me.com>
2024-05-03 22:51:38 +0200

    Events
    Updated event calendar

--- assets/js/randommessage.js
+++ assets/js/randommessage.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const messages = [
 "<strong>New tool!</strong> Use generative AI to learn all about Estedic. 👉🏼 Click on the 'chat with us' button on the top right.",
-"🗓️ <strong>April 24th:</strong> Discover the future of AI at the Dutch AI Congress. 👉🏼 <a href='https://www.estedic.nl/company.html#eventcalendar' style='color: white;'>Learn more</a>.",
+"🗓️ <strong>End of May:</strong> Stay tuned for our webinar in collaboration with data.world. 👉🏼 <a href='https://www.data.world' target='blank'; style='color: white;'>More on data.world</a>.",
+"🗓️ <strong>June 27:</strong> Estedic will attend the Snowflake Benelux Partner Connect 2024 in Amsterdam. 👉🏼 <a href='https://www.snowflake.com' target='blank'; style='color: white;'>More on Snowflake</a>.",
         // ... add more messages as needed
     ];
 
