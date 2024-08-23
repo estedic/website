fd854e9f3cf02033ee81cce03b35363967e423cc
Bram Bregman <bregman@me.com>
2024-08-23 23:49:28 +0200

    Added data expo visit
    Made some textual changes

--- assets/js/randommessage.js
+++ assets/js/randommessage.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const messages = [
 "<strong>New tool!</strong> Use generative AI to learn all about Estedic. 👉🏼 Click on the 'chat with us' button on the top right.",
 "🗓️ <strong>Stay tuned</strong> for our webinar in collaboration with data.world (final date t.b.d.). 👉🏼 <a href='https://www.data.world' target='blank'; style='color: white;'>More on data.world</a>.",
-"🗓️ <strong>June 27:</strong> Estedic will attend the Snowflake Benelux Partner Connect 2024 in Amsterdam. 👉🏼 <a href='https://www.snowflake.com' target='blank'; style='color: white;'>More on Snowflake</a>.",
-"🗓️ <strong>July 2nd:</strong> Estedic will attend the Databricks Benelux Partner Connect meet-up in Amsterdam. 👉🏼 <a href='https://www.databricks.com' target='blank'; style='color: white;'>More on Databricks</a>.",
-"🗓️ <strong>July 11th:</strong> Estedic will host a data strategy workshop at the Amsterdam Data Event. 👉🏼 <a href='https://www.amsterdamdataacademy.com' target='blank'; style='color: white;'>More on Amsterdam Data Academy</a>.",
+"🗓️ <strong>Meet us</strong> at the Data Expo on Sept 11th in Utrecht. 👉🏼 <a href='https://www.data-expo.nl/nl/' target='blank'; style='color: white;'>click here</a>.",
         // ... add more messages as needed
     ];
 
