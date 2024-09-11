a1706d825dc162c27bf2297fb484ec6429f1dfb3
Bram Bregman <bregman@me.com>
2024-09-11 20:43:12 +0200

    Changed calendar events

--- assets/js/randommessage.js
+++ assets/js/randommessage.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const messages = [
 "<strong>New tool!</strong> Use generative AI to learn all about Estedic. 👉🏼 Click on the 'chat with us' button on the top right.",
 "🗓️ <strong>Stay tuned</strong> for our webinar in collaboration with data.world (final date t.b.d.). 👉🏼 <a href='https://www.data.world' target='blank'; style='color: white;'>More on data.world</a>.",
-"🗓️ <strong>Meet us</strong> at the Data Expo on Sept 11th in Utrecht. 👉🏼 <a href='https://www.data-expo.nl/nl/' target='blank'; style='color: white;'>click here</a>.",
         // ... add more messages as needed
     ];
 
